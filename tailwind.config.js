66a9bddab55936c902caa5e2b97d8cd49c707b74
moashraf86 <botoashraf123@gmail.com>
2025-05-16 22:53:33 +0300

    chore: update package version to 1.0.0 and add tailwindcss-animate dependency
    feat: enhance styling with new utility classes and animations
    
    style: improve input.css and output.css for better layout and transitions
    
    config: extend tailwind configuration for custom transition durations and box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,31 @@ module.exports = {
   content: ["./**/*.{html,js}"],
 
   theme: {
-    extend: {},
-
+    extend: {
+      transitionDuration: {
+        250: "250ms",
+        350: "350ms",
+        400: "400ms",
+        450: "450ms",
+        550: "550ms",
+        600: "600ms",
+        650: "650ms",
+        750: "750ms",
+        800: "800ms",
+        850: "850ms",
+        900: "900ms",
+        950: "950ms",
+      },
+    },
+    boxShadow: {
+      card: "rgba(0, 0, 0, 0.05) 0px 2px 10px 0px",
+      cover:
+        "rgba(0, 0, 0, 0.03) -7px -5px 16px 0px, rgba(0, 0, 0, 0.03) 0px 17px 11px 0px",
+    },
     fontFamily: {
       outfit: ["Outfit", "sans-serif"],
     },
   },
   darkMode: "selector",
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
