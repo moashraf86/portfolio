d020169025c5132237bfc06566e7282d269da329
moashraf86 <botoashraf123@gmail.com>
2025-05-16 06:37:55 +0300

    chore: update project to version 1.1.0 with Tailwind CSS configuration changes
    - Updated package-lock.json and package.json to reflect version 1.1.0
    - Renamed build script to watch in package.json
    - Refactored input.css to use Outfit font and removed unused styles
    - Updated output.css to remove deprecated styles and added new utility classes
    - Modified tailwind.config.js to change font family to Outfit and adjusted content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // content: ["./src/**/*.{html,js}"],
   content: ["./**/*.{html,js}"],
 
   theme: {
     extend: {},
-    boxShadow: {
-      inset: "inset 0 1px 0 0 #ffffff0d",
-    },
+
     fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-      work: ["Work Sans", "sans-serif"],
-      caveat: ["Caveat", "cursive"],
+      outfit: ["Outfit", "sans-serif"],
     },
   },
   darkMode: "selector",
